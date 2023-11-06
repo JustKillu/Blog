707a583616ab092e3bd4474ee3a74fb79b1a098c
JustKillu <nightcorekillu@gmail.com>
2023-11-05 22:29:25 -0400

    initial commit

---
+++ scripts/submit.php
@@ -0,0 +1,29 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "blog";
+
+// Crear conexión
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Verificar conexión
+if ($conn->connect_error) {
+  die("Conexión fallida: " . $conn->connect_error);
+}
+
+$autor = $_POST['autor'];
+$correo = $_POST['correo'];
+$sugerencia = $_POST['sugerencia'];
+
+$sql = "INSERT INTO entradas (`Autor`, `Correo Electronico`, `Sugerencia`)
+VALUES ('$autor', '$correo', '$sugerencia')";
+
+if ($conn->query($sql) === TRUE) {
+  header("Location: ../news.php");
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
